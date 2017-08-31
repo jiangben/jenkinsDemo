9dc1fbba968d066789ba06f677d065b680aca5a7
jiangben <47379449@qq.com>
2017-08-31 17:48:19 +0800

    非致命判断

--- GoogleTest/main.cpp
+++ GoogleTest/main.cpp
@@ -20,23 +20,23 @@ public:
 };
 
 TEST_F(CalculatorTest, testadd){
-	ASSERT_EQ(9, m_calculator->add(5, 4));
+	EXPECT_EQ(9, m_calculator->add(5, 4));
 }
 
 TEST_F(CalculatorTest, testsub){	
-	ASSERT_EQ(4, m_calculator->sub(6, 2));
+	EXPECT_EQ(4, m_calculator->sub(6, 2));
 }
 
 TEST(TestCalculator, testALL)
 {
 	Calculator calculator;
-	ASSERT_EQ(12, calculator.add(2, 0));
-	ASSERT_EQ(16, calculator.add(2, 4));
-	ASSERT_EQ(17, calculator.add(2, 5));
+	EXPECT_EQ(12, calculator.add(2, 0));
+	EXPECT_EQ(16, calculator.add(2, 4));
+	EXPECT_EQ(17, calculator.add(2, 5));
 	
-	ASSERT_EQ(17, calculator.sub(7, 0));
-	ASSERT_EQ(15, calculator.sub(9, 4));
-	ASSERT_EQ(10, calculator.sub(5, 5));
+	EXPECT_EQ(17, calculator.sub(7, 0));
+	EXPECT_EQ(15, calculator.sub(9, 4));
+	EXPECT_EQ(10, calculator.sub(5, 5));
 }
 
 
